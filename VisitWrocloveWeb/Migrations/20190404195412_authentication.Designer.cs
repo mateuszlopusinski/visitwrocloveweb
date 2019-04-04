dea87d665e99527a775ee9df25b3f5b0d26369a6
Mateusz Lopusinski <mateusz.lopusinski2@gmail.com>
2019-04-04 21:56:13 +0200

    authentication migration

--- VisitWrocloveWeb/Migrations/20190404195412_authentication.Designer.cs
+++ VisitWrocloveWeb/Migrations/20190404195412_authentication.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VisitWrocloveWeb.Models;
 
 namespace VisitWrocloveWeb.Migrations
 {
     [DbContext(typeof(VisitWrocloveWebContext))]
-    partial class VisitWrocloveWebContextModelSnapshot : ModelSnapshot
+    [Migration("20190404195412_authentication")]
+    partial class authentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VisitWrocloveWeb/Migrations/20190404195412_authentication.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VisitWrocloveWeb.Migrations
+{
+    public partial class authentication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccessFailedCount",
+                table: "User",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailConfirmed",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LockoutEnabled",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LockoutEnd",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedEmail",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedUserName",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PhoneNumberConfirmed",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenCreatedDate",
+                table: "User",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryDate",
+                table: "User",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecurityStamp",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessFailedCount",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmed",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnabled",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnd",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedEmail",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedUserName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumberConfirmed",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenCreatedDate",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryDate",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "SecurityStamp",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorEnabled",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "User");
+        }
+    }
+}
