56228db4a865456e6b499bd03beacef0b288e407
Mateusz Lopusinski <mateusz.lopusinski2@gmail.com>
2019-04-04 18:02:13 +0200

    Add project files.

---
+++ VisitWrocloveWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VisitWrocloveWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
