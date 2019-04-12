507af8b23dc9891cb8d757b9f73fab29d7b9fc0f
Mateusz Lopusinski <mateusz.lopusinski2@gmail.com>
2019-04-12 20:16:52 +0200

    Changed image type to srting (url)

---
+++ VisitWrocloveWeb/Migrations/20190412180820_ImageChangedToString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VisitWrocloveWeb.Migrations
+{
+    public partial class ImageChangedToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "PlaceEvent",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "PlaceEvent",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- VisitWrocloveWeb/Models/PlaceEvent.cs
+++ VisitWrocloveWeb/Models/PlaceEvent.cs
@@ -9,7 +9,7 @@ namespace VisitWrocloveWeb.Models
         
         public string Description { get; set; }
 
-        public byte[] Image { get; set; }
+        public string Image { get; set; }
 
         public string Name { get; set; }
 
