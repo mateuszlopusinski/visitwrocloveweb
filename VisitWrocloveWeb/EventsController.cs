5b509cea3e4241aa84105a5252f5cbf5cbc1a7d6
Mateusz Lopusinski <mateusz.lopusinski2@gmail.com>
2019-04-27 12:13:13 +0200

    front for events and addresses

---
+++ VisitWrocloveWeb/EventsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VisitWrocloveWeb.Models;
+
+namespace VisitWrocloveWeb
+{
+    [AllowAnonymous]
+    public class EventsController : Controller
+    {
+        private readonly VisitWrocloveWebContext _context;
+
+        public EventsController(VisitWrocloveWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Events
+        public async Task<IActionResult> Index()
+        {
+            var visitWrocloveWebContext = _context.Event.Include(a => a.Address);
+            return View(await visitWrocloveWebContext.ToListAsync());
+        }
+
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .Include(e => e.Address)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id");
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventDateTime,Price,Id,Description,Image,Name,Thumbnail,Website,AddressForeignKey")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id", @event.AddressForeignKey);
+            return View(@event);
+        }
+
+        // GET: Events/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id", @event.AddressForeignKey);
+            return View(@event);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventDateTime,Price,Id,Description,Image,Name,Thumbnail,Website,AddressForeignKey")] Event @event)
+        {
+            if (id != @event.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(@event.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id", @event.AddressForeignKey);
+            return View(@event);
+        }
+
+        // GET: Events/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .Include(e => e.Address)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Event.FindAsync(id);
+            _context.Event.Remove(@event);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Event.Any(e => e.Id == id);
+        }
+    }
+}
