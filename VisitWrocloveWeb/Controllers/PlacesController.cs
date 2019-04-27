6e00e7fe0328d0ffe9d57feb8de9ad172ab6aace
Mateusz Lopusinski <mateusz.lopusinski2@gmail.com>
2019-04-27 12:25:17 +0200

    Places & reviews front

---
+++ VisitWrocloveWeb/Controllers/PlacesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VisitWrocloveWeb.Models;
+
+namespace VisitWrocloveWeb
+{
+    [AllowAnonymous]
+    public class PlacesController : Controller
+    {
+        private readonly VisitWrocloveWebContext _context;
+
+        public PlacesController(VisitWrocloveWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Places
+        public async Task<IActionResult> Index()
+        {
+            var visitWrocloveWebContext = _context.Place.Include(p => p.Address);
+            return View(await visitWrocloveWebContext.ToListAsync());
+        }
+
+        // GET: Places/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var place = await _context.Place
+                .Include(p => p.Address)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            return View(place);
+        }
+
+        // GET: Places/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id");
+            return View();
+        }
+
+        // POST: Places/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MinPrice,OpeningHours,Id,Description,Image,Name,Thumbnail,Website,AddressForeignKey")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(place);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id", place.AddressForeignKey);
+            return View(place);
+        }
+
+        // GET: Places/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var place = await _context.Place.FindAsync(id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id", place.AddressForeignKey);
+            return View(place);
+        }
+
+        // POST: Places/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MinPrice,OpeningHours,Id,Description,Image,Name,Thumbnail,Website,AddressForeignKey")] Place place)
+        {
+            if (id != place.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(place);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlaceExists(place.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressForeignKey"] = new SelectList(_context.Address, "Id", "Id", place.AddressForeignKey);
+            return View(place);
+        }
+
+        // GET: Places/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var place = await _context.Place
+                .Include(p => p.Address)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (place == null)
+            {
+                return NotFound();
+            }
+
+            return View(place);
+        }
+
+        // POST: Places/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var place = await _context.Place.FindAsync(id);
+            _context.Place.Remove(place);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlaceExists(int id)
+        {
+            return _context.Place.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ VisitWrocloveWeb/Controllers/ReviewsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VisitWrocloveWeb.Models;
+
+namespace VisitWrocloveWeb
+{
+    [AllowAnonymous]
+    public class ReviewsController : Controller
+    {
+        private readonly VisitWrocloveWebContext _context;
+
+        public ReviewsController(VisitWrocloveWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reviews
+        public async Task<IActionResult> Index()
+        {
+            var visitWrocloveWebContext = _context.Review.Include(r => r.PlaceEvent).Include(r => r.User);
+            return View(await visitWrocloveWebContext.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review
+                .Include(r => r.PlaceEvent)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public IActionResult Create()
+        {
+            ViewData["PlaceEventId"] = new SelectList(_context.Set<PlaceEvent>(), "Id", "Discriminator");
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Mark,UserId,PlaceEventId")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlaceEventId"] = new SelectList(_context.Set<PlaceEvent>(), "Id", "Discriminator", review.PlaceEventId);
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id", review.UserId);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["PlaceEventId"] = new SelectList(_context.Set<PlaceEvent>(), "Id", "Discriminator", review.PlaceEventId);
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id", review.UserId);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Mark,UserId,PlaceEventId")] Review review)
+        {
+            if (id != review.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlaceEventId"] = new SelectList(_context.Set<PlaceEvent>(), "Id", "Discriminator", review.PlaceEventId);
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id", review.UserId);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Review
+                .Include(r => r.PlaceEvent)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var review = await _context.Review.FindAsync(id);
+            _context.Review.Remove(review);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return _context.Review.Any(e => e.Id == id);
+        }
+    }
+}
